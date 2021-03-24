fa2acb85845755a271ccb88805381452e3096d31
RafaNM <rnadal@cifpfbmoll.eu>
2021-03-24 12:53:22 +0100

    Config.env y config/db.js

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = requirre ('mongoose')
+
+const connectDB = async () => {
+
+try{
+
+	const conn = await mongoose.connect(process.env.MONGO_URI, {
+
+		useNEWuRLpARSER: true, 
+
+		useUnifiedTopology: true,
+
+		useFindAndModify: false})
+
+console.log(`MongoDB Connected: ${conn.connection.host}`)
+
+}catch (err) {
+
+	console.error (err)
+
+	process.exit (1)
+
+}
+}
+
+module.exports = connectDB
