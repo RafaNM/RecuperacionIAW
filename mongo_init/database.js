cfb71d99965cc55596c83f54f9381505d878bc37
RafaNM <72399004+RafaNM@users.noreply.github.com>
2021-06-11 11:16:50 +0200

    Add files via upload

---
+++ mongo_init/database.js
@@ -0,0 +1,403 @@
+// db.js
+
+db.hotels.insert([
+    {
+        "name": "Hotel Hospes Maricel & Spa",
+        "verified": true,
+        "web": "https://www.hospes.com/maricel-spa/",
+        "phone": 914363478,
+        "email": "reservations@hospes.com",
+        "stars": 5,
+        "price": 400,
+        "location": "Ctra. Andratx, 11, 07181 Palma, Illes Balears",
+        "rooms": [
+            {
+                "Regular": [
+                    {
+                        "room": "Individual",
+                        "price": 150,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Double",
+                        "price": 200,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Deluxe",
+                        "price": 250,
+                        "coin": "EUR"
+                    }
+                ]
+            },
+            {
+                "VIP": [
+                    {
+                        "room": "Suite",
+                        "price": 400,
+                        "coin": "EUR"
+                    }
+                ]
+            }
+        ],
+        "schedule": [
+            {
+                "Monday": "8:00h - 00:00h"
+            },
+            {
+                "Tuesday": "8:00h - 00:00h"
+            },
+            {
+                "Wednesday": "8:00h - 00:00h"
+            },
+            {
+                "Thursday": "8:00h - 00:00h"
+            },
+            {
+                "Friday": "8:00h - 00:00h"
+            },
+            {
+                "Saturday": "8:00h - 18:00h"
+            },
+            {
+                "Sunday": "8:00h - 16:00h"
+            }
+        ],
+        "type": "beach and family",
+        "prating": "2",
+        "image": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/4e/9c/ed/restaurant.jpg?w=900&h=-1&s=1"
+    },
+    {
+        "name": "Hotel BLUESEA Arenal Tower",
+        "verified": true,
+        "web": "https://www.blueseahotels.com/hoteles/destinos/mallorca/el-arenal/blue-sea-hotel-arenal-tower-adults-only?utm_source=mybusiness&utm_medium=organic",
+        "phone": 971440850,
+        "email": "hotelarenaltower@blueseahotels.es",
+        "stars": 3,
+        "price": 52,
+        "location": "C/ Baleares, 2 07600 Llucmajor, Mallorca (Illes Balears), Spain",
+        "rooms": [
+            {
+                "Regular": [
+                    {
+                        "room": "Individual",
+                        "price": 50,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Double",
+                        "price": 100,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Deluxe",
+                        "price": 120,
+                        "coin": "EUR"
+                    }
+                ]
+            },
+            {
+                "VIP": [
+                    {
+                        "room": "Suite",
+                        "price": 180,
+                        "coin": "EUR"
+                    }
+                ]
+            }
+        ],
+        "schedule": [
+            {
+                "Monday": "8:00h - 00:00h"
+            },
+            {
+                "Tuesday": "8:00h - 00:00h"
+            },
+            {
+                "Wednesday": "8:00h - 00:00h"
+            },
+            {
+                "Thursday": "8:00h - 00:00h"
+            },
+            {
+                "Friday": "8:00h - 00:00h"
+            },
+            {
+                "Saturday": "8:00h - 18:00h"
+            },
+            {
+                "Sunday": "8:00h - 16:00h"
+            }
+        ],
+        "type": "beach and family",
+        "prating": "5",
+        "image": "https://pix10.agoda.net/hotelImages/850/850284/850284_16060120430043022042.jpg"
+    },
+    {
+        "name": "PortBlue Club Pollentia Resort & Spa",
+        "verified": true,
+        "web": "https://www.portbluehotels.com/hoteles/portblue-club-pollentia-resort-and-spa/?utm_source=google&utm_medium=organic&utm_campaign=gmb&utm_term=clubpollentia&partner=4182",
+        "phone": 971897430,
+        "email": "call-center@portblue.com",
+        "stars": 4,
+        "price": 115,
+        "location": "Ctra. Alcúdia, Km 2, 07400, Illes Balears",
+        "rooms": [
+            {
+                "Regular": [
+                    {
+                        "room": "Individual",
+                        "price": 100,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Double",
+                        "price": 160,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Deluxe",
+                        "price": 230,
+                        "coin": "EUR"
+                    }
+                ]
+            },
+            {
+                "VIP": [
+                    {
+                        "room": "Suite",
+                        "price": 290,
+                        "coin": "EUR"
+                    }
+                ]
+            }
+        ],
+        "schedule": [
+            {
+                "Monday": "8:00h - 00:00h"
+            },
+            {
+                "Tuesday": "8:00h - 00:00h"
+            },
+            {
+                "Wednesday": "8:00h - 00:00h"
+            },
+            {
+                "Thursday": "8:00h - 00:00h"
+            },
+            {
+                "Friday": "8:00h - 00:00h"
+            },
+            {
+                "Saturday": "8:00h - 18:00h"
+            },
+            {
+                "Sunday": "8:00h - 16:00h"
+            }
+        ],
+        "type": "beach and family",
+        "prating": "3",
+        "image": "https://www.portbluehotels.com/wp-content/uploads/2018/12/DJI_0146-HDR.jpg"
+    },
+    {
+        "name": "Hotel Illa d´Or",
+        "verified": true,
+        "web": "https://www.hotelillador.com/es/",
+        "phone": 971865100,
+        "email": "illador@hoposa.es",
+        "stars": 4,
+        "price": 162,
+        "location": "Calle Colón, 265, 07470 Pollença, Balearic Islands",
+        "rooms": [
+            {
+                "Regular": [
+                    {
+                        "room": "Individual",
+                        "price": 120,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Double",
+                        "price": 180,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Deluxe",
+                        "price": 250,
+                        "coin": "EUR"
+                    }
+                ]
+            },
+            {
+                "VIP": [
+                    {
+                        "room": "Suite",
+                        "price": 300,
+                        "coin": "EUR"
+                    }
+                ]
+            }
+        ],
+        "schedule": [
+            {
+                "Monday": "8:00h - 00:00h"
+            },
+            {
+                "Tuesday": "8:00h - 00:00h"
+            },
+            {
+                "Wednesday": "8:00h - 00:00h"
+            },
+            {
+                "Thursday": "8:00h - 00:00h"
+            },
+            {
+                "Friday": "8:00h - 00:00h"
+            },
+            {
+                "Saturday": "8:00h - 18:00h"
+            },
+            {
+                "Sunday": "8:00h - 16:00h"
+            }
+        ],
+        "type": "beach and family",
+        "prating": "1",
+        "image": "https://www.icastelli.net/photo/images/hotel-illa-dor_1000_560_1370_1440407875.jpg"
+    },
+    {
+        "name": "Citric Hotel Sóller",
+        "verified": true,
+        "web": "https://citrichotels.com/es/",
+        "phone": 971631352,
+        "email": "info@citrichotels.com",
+        "stars": 1,
+        "price": 56,
+        "location": "Camí del Far, s/n, 07100, Illes Balears",
+        "rooms": [
+            {
+                "Regular": [
+                    {
+                        "room": "Individual",
+                        "price": 50,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Double",
+                        "price": 90,
+                        "coin": "EUR"
+                    },
+                    {
+                        "room": "Deluxe",
+                        "price": 105,
+                        "coin": "EUR"
+                    }
+                ]
+            },
+            {
+                "VIP": [
+                    {
+                        "room": "Suite",
+                        "price": 150,
+                        "coin": "EUR"
+                    }
+                ]
+            }
+        ],
+        "schedule": [
+            {
+                "Monday": "8:00h - 00:00h"
+            },
+            {
+                "Tuesday": "8:00h - 00:00h"
+            },
+            {
+                "Wednesday": "8:00h - 00:00h"
+            },
+            {
+                "Thursday": "8:00h - 00:00h"
+            },
+            {
+                "Friday": "8:00h - 00:00h"
+            },
+            {
+                "Saturday": "8:00h - 18:00h"
+            },
+            {
+                "Sunday": "8:00h - 16:00h"
+            }
+        ],
+        "type": "beach and family",
+        "prating": "4",
+        "image": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/9b/4d/83/citric-hotel-soller.jpg?w=900&h=-1&s=1"
+    }
+])
+
+db.reviews.insert([{
+    "_id": "rev1",
+    "date": "2020-09-15",
+    "urating": 4,
+    "title": "It costs just two dollars!",
+    "review": "We drove up to the drive-up and we ordered 2 Mcchicken sandwiches...",
+    "restaurant": "res1",
+    "user": "use1"
+},
+{
+    "_id": "rev2",
+    "date": "2020-11-06",
+    "urating": 4,
+    "title": "Ain't got no tears left to cry",
+    "review": "2 filet-O-fish, 2 med fries and 1 med lemonade...",
+    "restaurant": "res2",
+    "user": "use2"
+},
+{
+    "_id": "rev3",
+    "date": "2020-08-05",
+    "urating": 5,
+    "title": "Dream of meet",
+    "review": "There were two of us, we drove around to park and eat in our car...",
+    "restaurant": "res3",
+    "user": "use3"
+},
+{
+    "_id": "rev4",
+    "date": "2020-09-17",
+    "urating": 2,
+    "title": "Creamy and creamy, vegetable juice",
+    "review": "We opened the fish sandwiches and we were so disappointed...",
+    "restaurant": "res4",
+    "user": "use4"
+},
+{
+    "_id": "rev5",
+    "date": "2020-09-26",
+    "urating": 4,
+    "title": "Have some ham, you will see what great pleasure",
+    "review": "Real fat, good quality...",
+    "restaurant": "res5",
+    "user": "use1"
+}])
+
+db.users.insert([
+  {
+      "_id": "use1",
+      "username": "Gloria",
+      "city": "Vancouver"
+  },
+  {
+      "_id": "use2",
+      "username": "Virginia",
+      "city": "Guanajuato"
+  },
+  {
+      "_id": "use3",
+      "username": "Francisco",
+      "city": "Turín"
+  },
+  {
+      "_id": "use4",
+      "username": "Maria Fulgensia",
+      "city": "Vilna"
+  }
+])
